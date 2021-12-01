3f92dfcb8524570939c4dd50ff4082976dc7dab0
MHJworld <mhj960808@gmail.com>
2021-12-01 17:16:46 +0900

    second commit

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 import boto3
 
+def Menu():
+    print('-------------------------------------------------\n')
+    print('  1. list instance       2. available zones\n')
+    print('  3. start instance      4. available regions\n')
+    print('  5. stop instance       6. create instance\n')
+    print('  7. reboot instance     8. list images\n')
+    print('                         99. quit\n')
+    print('-------------------------------------------------\n')
+    
